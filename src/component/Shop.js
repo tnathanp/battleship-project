02545958b173f87d4dfda8020e3bf7630a2d0b7a
Janettre Kaivipakbanyai <janettrek_@outlook.com>
2020-10-29 22:51:17 +0700

    add Shop

--- src/component/Shop.js
+++ src/component/Shop.js
@@ -10,32 +10,51 @@ class Shop extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            roomList: [],
-            friendID: '',
-            roomID: '',
-            showInviteFriend: false,
-            showCreateRoom: false
+            pocket:0,
+            missileOwned:0,
+            glassesOwned:0
         }
     }
 
-
     render() {
-        
-     
-
         return (
             <Container>
                 <Row>
                     <Col>
+                        <Card>
+                            <Card.Body>Pocket: {this.state.pocket}</Card.Body>
+                        </Card>
                         <Row>
                             <Col>
-                            Item 1
+                                <Card>
+                                    <Card.Body class="text-center">
+                                        <Card.Img variant="top" src="https://media.istockphoto.com/vectors/startup-icon-vector-id1074164928?k=6&m=1074164928&s=612x612&w=0&h=dD2gAKmO5MNG-eOh2WE34ZMoLSpF0j_YSYvTFKl-FfA=" />
+                                        <Card.Title style= {{marginTop: '20px'}} >Five-shot Missile</Card.Title>
+                                        <Card.Text style= {{marginBottom: '20px'}}>Strike five squares in a cross pattern on the grid in one turn</Card.Text>
+                                        <Card.Text>Currently owned: {this.state.missileOwned}</Card.Text>
+                                        <Card.Text style= {{marginTop: '-15px'}}>Price: 100</Card.Text>
+                                        <Card.Text style= {{marginBottom: '0px'}}>Purchase:</Card.Text>
+                                        <Button variant="outline-primary" style= {{width: '100px', marginRight: '10px'}}>X1</Button>
+                                        <Button variant="outline-primary" style= {{width: '100px'}}>X10</Button>
+                                    </Card.Body>
+                                </Card>
                             </Col>
                         
 
                         
                             <Col>
-                            Item 2
+                                <Card>
+                                    <Card.Body class="text-center">
+                                        <Card.Img variant="top" src="https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-ios7-glasses-outline-512.png" />
+                                        <Card.Title style= {{marginTop: '20px'}} >Glasses</Card.Title>
+                                        <Card.Text style= {{marginBottom: '20px'}}>Randomly unviel one square</Card.Text>
+                                        <Card.Text>Currently owned: {this.state.glassesOwned}</Card.Text>
+                                        <Card.Text style= {{marginTop: '-15px'}}>Price: 50</Card.Text>
+                                        <Card.Text style= {{marginBottom: '0px'}}>Purchase:</Card.Text>
+                                        <Button variant="outline-primary" style= {{width: '100px', marginRight: '10px'}}>X1</Button>
+                                        <Button variant="outline-primary" style= {{width: '100px'}}>X10</Button>
+                                    </Card.Body>
+                                </Card>
                             </Col>
                         </Row>
                     
