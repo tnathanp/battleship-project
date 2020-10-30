6768741438c48d029d01dcb8af56931a60effd6d
fifatanathan <tanathanp@gmail.com>
2020-10-31 03:22:28 +0700

    Sending auth key along with connection

--- src/connection.js
+++ src/connection.js
@@ -1,3 +1,3 @@
 import server from 'socket.io-client';
-let socket = server('https://battleship-server.azurewebsites.net');
+let socket = server('https://battleship-server.azurewebsites.net?auth=' + localStorage.getItem('auth'));
 export default socket;
