58478bee8ca3f78eae7f685d1385d32133a71025
fifatanathan <tanathanp@gmail.com>
2020-11-03 10:17:10 +0700

    Trying heroku service

--- src/connection.js
+++ src/connection.js
@@ -1,3 +1,3 @@
 import server from 'socket.io-client';
-let socket = server('https://battleship-server.azurewebsites.net?auth=' + localStorage.getItem('auth'));
+let socket = server('http://battleship-server-a01.herokuapp.com?auth=' + localStorage.getItem('auth'));
 export default socket;
