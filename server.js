300621eae4af4b340c801d4b8b6d96bc2bcdaa75
fifatanathan <tanathanp@gmail.com>
2020-11-03 20:38:16 +0700

    Admin function

--- server.js
+++ server.js
@@ -50,6 +50,7 @@ server.on('connection', socket => {
                 console.log(sockets[id]['handshake']['query']['auth'] + ' on ' + id);
         }
         console.log('\n');
+        server.emit('update admin console');
     }
 
     //User validation
@@ -94,7 +95,7 @@ server.on('connection', socket => {
                 db.close();
             })
         })
-
+        server.emit('update admin console');
     });
 
     socket.on('change profile', data => {
@@ -107,12 +108,14 @@ server.on('connection', socket => {
                 db.close()
             })
         })
+        server.emit('update admin console');
     })
 
     //Connection close
     socket.on('offline', auth => {
         socket.disconnect();
     })
+
     socket.on('disconnect', () => {
         let sockets = server.sockets.sockets;
         console.log('Current online list');
@@ -121,16 +124,41 @@ server.on('connection', socket => {
                 console.log(sockets[id]['handshake']['query']['auth'] + ' on ' + id);
         }
         console.log('\n');
-        //then the client auto leave room, so we have to remove room from the list
+        server.emit('update room list', roomList);
+        server.emit('update admin console');
+    })
 
-        if (roomList.length !== 0) for (let i = 0; i <= roomList.length; i++) {
-            if (roomList[i]['socketID'] === socket.id) {
-                roomList.splice(i, 1);
-                server.emit('update room list', roomList);
-                break;
+    socket.on('disconnecting', () => {
+        let roomConnection = server.sockets.adapter.rooms
+        for (let key in roomConnection) {
+            if (roomConnection[key].length === 2) {
+                if (socket.id === Object.keys(roomConnection[key]['sockets'])[0] ||
+                    socket.id === Object.keys(roomConnection[key]['sockets'])[1]) {
+                    roomList = roomList.filter(each => {
+                        if (each.roomID === key) {
+                            return false;
+                        }
+                        return true;
+                    })
+                    socket.broadcast.to(key).emit('opponent disconnect');
+                }
+            } else {
+                if (socket.id === Object.keys(roomConnection[key]['sockets'])[0]) {
+                    roomList = roomList.filter(each => {
+                        if (each.roomID === key) {
+                            return false;
+                        }
+                        return true;
+                    })
+                }
             }
         }
-        server.emit('update room list', roomList);
+        socket.leaveAll();
+    })
+
+    socket.on('leave room', () => {
+        socket.leaveAll();
+        server.emit('update admin console');
     })
 
     socket.on('request user data', auth => {
@@ -158,15 +186,14 @@ server.on('connection', socket => {
 
     socket.on('join invitation', room => {
         if (server.sockets.adapter.rooms[room] != null) {
-           // console.log(server.sockets.adapter.rooms[room].length)
             if (server.sockets.adapter.rooms[room].length === 1) {
                 socket.join(room);
-               // console.log(server.sockets.adapter.rooms[room].length)
                 socket.to(room).broadcast.emit('join invitation success')
             } else {
                 socket.to(room).broadcast.emit('join invitation fail')
             }
         }
+        server.emit('update admin console');
     })
 
     socket.on('request friend id', friendID => {
@@ -240,6 +267,7 @@ server.on('connection', socket => {
         }
         console.log('\n current room list');
         console.log(roomList);
+        server.emit('update admin console');
     })
 
     socket.on('get room list', () => {
@@ -269,21 +297,11 @@ server.on('connection', socket => {
         } else {
             console.log('error room not exists');
         }
+        server.emit('update admin console');
     })
 
-    socket.on('admin authorization', auth => {
-        if (auth === '710789ee-5918-4e44-a946-33ddacaab753')
-            socket.emit('admin authorized');
-        else
-            socket.emit('restricted access')
-    })
+    socket.on('get pocket', () => {
 
-    socket.on('admin request online list', data => {
-        socket.broadcast.to(data.roomID).emit('msg rcv', data.msg);
-    })
-
-    socket.on('get pocket',()=>{
-        
         MongoClient.connect(uri, function (err, db) {
             if (err) throw err;
             let conn = db.db("battleship-project").collection("user");
@@ -291,7 +309,7 @@ server.on('connection', socket => {
 
             conn.findOne({ auth: sockets[socket.id]['handshake']['query']['auth'] }, function (err, result) {
                 if (err) throw err;
-                socket.emit('response pocket', { pocket: result.pocket, missile: result.items.missile  ,glasses: result.items.glasses});
+                socket.emit('response pocket', { pocket: result.pocket, missile: result.items.missile, glasses: result.items.glasses });
                 /*console.log('pocket & items sent')
                 console.log('result' + result)
                 console.log('pocket: ' + result.pocket)
@@ -299,89 +317,202 @@ server.on('connection', socket => {
                 console.log('glasses: ' + result.items.glasses)*/
                 db.close();
             })
-       
+
         })
-        
+
     })
 
-    socket.on('buy glasses', data =>{
+    socket.on('buy glasses', data => {
         MongoClient.connect(uri, function (err, db) {
             if (err) throw err;
             let conn = db.db("battleship-project").collection("user");
-     
+
             let pocketUpdate = 0
             let glassesUpdate = 0
             conn.findOne({ auth: data.auth }, function (err, result) {
                 if (err) throw err;
                 //console.log('pocket: ' + result.pocket)
                 //console.log('glasses: ' + result.items.glasses)
-                 
-                pocketUpdate = Number(result.pocket) - 50* Number(data.num)
+
+                pocketUpdate = Number(result.pocket) - 50 * Number(data.num)
                 glassesUpdate = Number(result.items.glasses) + Number(data.num)
 
                 //console.log('pocket: ' + pocketUpdate)
                 //console.log('glasses: ' + glassesUpdate)
 
-                conn.updateOne({ auth: data.auth }, { $set: { 
-                    pocket: pocketUpdate,
-                    items: {
-                        missile: result.items.missile,
-                        glasses: glassesUpdate
-                    }  
-                }}, function (err, result) {
-                    socket.emit('response buy glasses', { pocket: pocketUpdate  ,glasses: glassesUpdate});
-                   // console.log('done buying ja')
-                  
-                   
+                conn.updateOne({ auth: data.auth }, {
+                    $set: {
+                        pocket: pocketUpdate,
+                        items: {
+                            missile: result.items.missile,
+                            glasses: glassesUpdate
+                        }
+                    }
+                }, function (err, result) {
+                    socket.emit('response buy glasses', { pocket: pocketUpdate, glasses: glassesUpdate });
+                    // console.log('done buying ja')
+
+
                 })
                 //console.log('pocket: ' +  result.pocket)
                 db.close();
             })
-            
-       
-       
         })
     })
 
-    socket.on('buy missile', data =>{
+    socket.on('buy missile', data => {
         MongoClient.connect(uri, function (err, db) {
             if (err) throw err;
             let conn = db.db("battleship-project").collection("user");
-     
+
             let pocketUpdate = 0
             let missileUpdate = 0
             conn.findOne({ auth: data.auth }, function (err, result) {
                 if (err) throw err;
                 console.log('pocket: ' + result.pocket)
                 console.log('missile: ' + result.items.missile)
-                 
-                pocketUpdate = Number(result.pocket) - 100* Number(data.num)
+
+                pocketUpdate = Number(result.pocket) - 100 * Number(data.num)
                 missileUpdate = Number(result.items.missile) + Number(data.num)
 
-              //  console.log('pocket: ' + pocketUpdate)
-              //  console.log('missile: ' + missileUpdate)
+                //  console.log('pocket: ' + pocketUpdate)
+                //  console.log('missile: ' + missileUpdate)
 
-                conn.updateOne({ auth: data.auth }, { $set: { 
-                    pocket: pocketUpdate,
-                    items: {
-                        missile: missileUpdate,
-                        glasses: result.items.glasses
-                    }  
-                }}, function (err, result) {
-                    socket.emit('response buy missile', { pocket: pocketUpdate  ,missile:  missileUpdate});
-                   // console.log('done buying ja')
-                  
-                   
+                conn.updateOne({ auth: data.auth }, {
+                    $set: {
+                        pocket: pocketUpdate,
+                        items: {
+                            missile: missileUpdate,
+                            glasses: result.items.glasses
+                        }
+                    }
+                }, function (err, result) {
+                    socket.emit('response buy missile', { pocket: pocketUpdate, missile: missileUpdate });
+                    // console.log('done buying ja')
                 })
-               // console.log('pocket: ' +  result.pocket)
+                // console.log('pocket: ' +  result.pocket)
                 db.close();
             })
-            
-       
-       
         })
     })
 
-    
+    socket.on('admin authorization', auth => {
+        if (auth === '72b66123-98b4-4d92-9719-2aca10a98713') {
+            let sockets = server.sockets.sockets;
+            let online = [];
+            let onlineWithData = {};
+
+            //get auth online list for database
+            //get auth online list with corespond socket id for room identification
+            for (let id in sockets) {
+                if (sockets[id]['handshake']['query']['auth'] != null && sockets[id]['handshake']['query']['auth'] != 'null') {
+                    online.push({
+                        auth: sockets[id]['handshake']['query']['auth']
+                    });
+                    onlineWithData[sockets[id]['handshake']['query']['auth']] = {
+                        socketID: id,
+                        joinedRoom: ''
+                    }
+                }
+            }
+
+            //identify the corespond room name of each online member
+            let roomConnection = server.sockets.adapter.rooms
+            for (let each in onlineWithData) {
+                for (let key in roomConnection) {
+                    if (roomConnection[key].length === 1) {
+                        if (Object.keys(roomConnection[key]['sockets'])[0] === key)
+                            continue;
+                        else if (Object.keys(roomConnection[key]['sockets'])[0] === onlineWithData[each]['socketID']) {
+                            onlineWithData[each]['joinedRoom'] = key;
+                            continue;
+                        }
+                    } else if (roomConnection[key].length === 2) {
+                        if (onlineWithData[each]['socketID'] === Object.keys(roomConnection[key]['sockets'])[0]) {
+                            onlineWithData[each]['joinedRoom'] = key;
+                            continue;
+                        } else if (onlineWithData[each]['socketID'] === Object.keys(roomConnection[key]['sockets'])[1]) {
+                            onlineWithData[each]['joinedRoom'] = key;
+                            continue;
+                        }
+                    }
+                }
+            }
+
+            //retrieve info of each auth from database and add room info to the result then send to client
+            MongoClient.connect(uri, function (err, db) {
+                if (err) throw err;
+                let conn = db.db("battleship-project").collection("user");
+
+                conn.find({ $or: online }).toArray(function (err, result) {
+                    for (let each of result) {
+                        if (each.auth in onlineWithData) {
+                            each.room = onlineWithData[each.auth]['joinedRoom'];
+                        }
+                    }
+                    socket.emit('admin authorized', result);
+                    db.close();
+                })
+            })
+        } else {
+            socket.emit('restricted access');
+        }
+    })
+
+    //admin tools
+    socket.on('add money', auth => {
+        MongoClient.connect(uri, function (err, db) {
+            if (err) throw err;
+            let conn = db.db("battleship-project").collection("user");
+
+            conn.updateOne({ auth: auth }, { $inc: { pocket: 1000 } }, function (err, result) {
+                server.emit('update admin console');
+                db.close();
+            })
+
+        })
+    })
+
+    socket.on('kick', auth => {
+        let sockets = server.sockets.sockets;
+        for (let id in sockets) {
+            if (sockets[id]['handshake']['query']['auth'] != null)
+                if (sockets[id]['handshake']['query']['auth'] == auth) {
+                    server.to(id).emit('get kicked');
+                    break;
+                }
+        }
+    })
+
+    socket.on('reset points', auth => {
+        MongoClient.connect(uri, function (err, db) {
+            if (err) throw err;
+            let conn = db.db("battleship-project").collection("user");
+
+            conn.updateOne({ auth: auth }, { $set: { points: 0 } }, function (err, result) {
+                server.emit('update admin console');
+                db.close();
+            })
+
+        })
+    })
+
+    socket.on('reset room', room => {
+        if (room !== '') {
+            server.of('/').in(room).clients((error, socketIds) => {
+                if (error) throw error;
+                socketIds.forEach(socketId => server.sockets.sockets[socketId].leave(room));
+            });
+
+            roomList = roomList.filter(each => {
+                if (each.roomID === room) {
+                    return false;
+                }
+                return true;
+            })
+            server.emit('update room list', roomList);
+            server.emit('update admin console');
+        }
+    })
 
 });
